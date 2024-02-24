ec0895691f5a20d2f41dd2335e8b205643bc2b0c
SPUCO <160882656+SPUCO@users.noreply.github.com>
2024-02-24 17:45:46 +0200

    Add files via upload

---
+++ admin/adminlayouts/adminfooter.php
@@ -0,0 +1,9 @@
+<!--------- Admin footer --------->
+  <div class="footer">
+	  <div class="container">
+		  <hr>
+		  <p class="copyright">Copyright 2024 - maryrosednk Pty Ltd</p>
+	  </div>
+  </div>
+  </body>
+</html>
